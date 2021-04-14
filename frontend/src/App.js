544e952a10bfceb7d7b555d88ae571cc11bbbeb1
Samdish <samdeeshsingh001@gmail.com>
2021-04-14 13:22:48 +0530

    featurette section for top rated foods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,34 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavMenu from "./components/NavMenu";
 import BgHome from "./components/BgHome";
+import Featurette from "./components/Featurette";
+import Container from "react-bootstrap/Container";
 
 function App() {
     return (
         <div className="App">
             <NavMenu />
             <BgHome />
+            <Container>
+                <h1 className="d-inline-block home-main-heading py-2 my-2">
+                    Our Top Rated <span> Meals.</span>{" "}
+                </h1>
+                <Featurette
+                    imgUrl="./assets/images/food/feature-1.jpg"
+                    imgOrder="2"
+                    contentOrder="1"
+                />
+                <Featurette
+                    imgUrl="./assets/images/food/feature-2.jpg"
+                    imgOrder="1"
+                    contentOrder="2"
+                />
+                <Featurette
+                    imgUrl="./assets/images/food/feature-3.jpg"
+                    imgOrder="2"
+                    contentOrder="1"
+                />
+            </Container>
         </div>
     );
 }
---
+++ frontend/src/components/Featurette.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../App.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+const Featurette = (props) => {
+    return (
+        <>
+            <Row className="d-flex justify-content-center align-items-center py-4">
+                <Col md={7} className={`order-md-${props.contentOrder}`}>
+                    <h2 className="featurette-heading text-info">
+                        Bread Omlet Salsa.{" "}
+                        <span className="text-muted">
+                            It’ll blow your mind.
+                        </span>
+                    </h2>
+                    <p className="lead">
+                        Some great placeholder content for the first featurette
+                        here. Imagine some exciting prose here.
+                    </p>
+                </Col>
+                <Col md={5} className={`order-md-${props.imgOrder}`}>
+                    <Image
+                        src={props.imgUrl}
+                        alt="feature"
+                        fluid
+                        roundedCircle
+                    />
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default Featurette;
