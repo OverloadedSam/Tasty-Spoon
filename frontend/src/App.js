d56ddd71117050ec32c4dd8c827f4f631cf20c75
Samdish <samdeeshsingh001@gmail.com>
2021-04-18 17:51:01 +0530

    refactors testimonials section: made a seperate component for Testimonials

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,9 @@ import NavMenu from "./components/NavMenu";
 import BgHome from "./components/BgHome";
 import Featurette from "./components/Featurette";
 import ChooseUs from "./components/ChooseUs";
-import ProfileCard from "./components/ProfileCard";
+import Testimonial from "./components/Testimonial";
 import Footer from "./components/Footer";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 
 function App() {
     return (
@@ -37,30 +36,7 @@ function App() {
             </Container>
 
             <ChooseUs />
-
-            <Container>
-                <h1 className="d-inline-block home-main-heading py-2 my-5">
-                    Our <span> Testimonials.</span>{" "}
-                </h1>
-
-                <Row>
-                    <ProfileCard
-                        customerName="John Doe"
-                        customerImg="./assets/images/testimonials/c1.jpg"
-                        testimonial="They are the best in every aspect of delivering good food and grocery."
-                    />
-                    <ProfileCard
-                        customerName="Dave"
-                        customerImg="./assets/images/testimonials/c2.jpg"
-                        testimonial="I randomly found them on internet and now i am their regular customer."
-                    />
-                    <ProfileCard
-                        customerName="Selena Gomez"
-                        customerImg="./assets/images/testimonials/c3.jpg"
-                        testimonial="The best brand for ordering any kind of meal, food or grocery in Delhi."
-                    />
-                </Row>
-            </Container>
+            <Testimonial />
             <Footer />
         </div>
     );
