66ef8b09d512ac1bb313506461fa730bd789d24a
Samdish <samdeeshsingh001@gmail.com>
2021-04-18 20:58:30 +0530

    converts all anchors (links) of NavMenu to React's Router Link

--- frontend/src/components/NavMenu.jsx
+++ frontend/src/components/NavMenu.jsx
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Badge from "react-bootstrap/Badge";
+import {Link} from 'react-router-dom'
 
 const NavMenu = () => {
     return (
@@ -16,18 +17,17 @@ const NavMenu = () => {
                     id="application-navbar"
                 >
                     <Nav className="ml-auto">
-                        <Nav.Link className="mx-auto px-3 navbar-item" href="#home">Home</Nav.Link>
-                        <Nav.Link className="mx-auto px-3 navbar-item" href="#link">About</Nav.Link>
-                        <Nav.Link className="mx-auto px-3 navbar-item" href="#link">Meals</Nav.Link>
-                        <Nav.Link className="mx-auto px-3 navbar-item" href="#link">
+                        <Nav.Link className="mx-auto px-3 navbar-item" as={ Link } to="/">Home</Nav.Link>
+                        <Nav.Link className="mx-auto px-3 navbar-item" as={ Link } to="/about">About</Nav.Link>
+                        <Nav.Link className="mx-auto px-3 navbar-item" as={ Link } to="/meal">Meal</Nav.Link>
+                        <Nav.Link className="mx-auto px-3 navbar-item" as={ Link } to="/grocery">
                             Grocery <Badge variant="danger">New</Badge>
                         </Nav.Link>
                         <NavDropdown className="mx-auto px-3 navbar-item" title="Categories" id="basic-nav-dropdown">
-                            <NavDropdown.Item className="navbar-item" href="#action/3.1">
-
+                            <NavDropdown.Item className="navbar-item" as={ Link } to="/foodcategories">
                                 <i className="fa fa-cutlery"> Food Categories</i>
                             </NavDropdown.Item>
-                            <NavDropdown.Item className="navbar-item" href="#action/3.2">
+                            <NavDropdown.Item className="navbar-item" as={ Link } to="/grocerycategories">
                                 <i className="fa fa-lemon-o"> Grocery Categories</i>
                             </NavDropdown.Item>
                         </NavDropdown>
@@ -38,18 +38,18 @@ const NavMenu = () => {
                             <hr style={{ width: "85%", height: "1px", border: "none", color: "#000", backgroundColor: "#000" }} />
                         </Nav.Item>
                         <NavDropdown className="mx-auto px-3 navbar-item" title="Sam" id="profile-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >
                                 <i className="fa fa-user-circle-o"> Samdeesh</i>
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item as={ Link } to="user/">
                                 <i className="fa fa-gear"><span> Profile</span></i>
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item as={ Link } to="/logout">
                                 <i className="fa fa-sign-out"><span> Logout</span></i>
                             </NavDropdown.Item>
                         </NavDropdown>
 
-                        <Nav.Link className="mx-auto px-3" href="#home">
+                        <Nav.Link className="mx-auto px-3" as={ Link } to="/cart">
                             <i className="fa fa-shopping-cart"><span className="navbar-item"> Cart</span></i>
                         </Nav.Link>
                     </Nav>
