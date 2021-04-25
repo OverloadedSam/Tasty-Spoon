ed591fb78bac634f1127b85a972c09ca8a14a00a
Samdish <samdeeshsingh001@gmail.com>
2021-04-25 10:29:14 +0530

    fixes navmenu for log in and user account dropdown menu

--- frontend/src/components/NavMenu.jsx
+++ frontend/src/components/NavMenu.jsx
@@ -3,10 +3,28 @@ import "../css/navMenuStyle.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import SignInModal from './SignInModal';
 import Badge from "react-bootstrap/Badge";
 import {Link} from 'react-router-dom'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
+import {useSelector, useDispatch} from 'react-redux';
+import {signOut} from '../redux/actions/userActions'
 
 const NavMenu = () => {
+    const dispatch = useDispatch();
+    const userSignIn = useSelector(state => state.userSignIn);
+    const {userData} = userSignIn;
+
+    var userFirstName;
+    if(userData){
+        userFirstName = userData.userFirstName;
+    }
+    const signOutHandler = () => {
+        dispatch(signOut());
+    }
+    const [showSignInModal, setShowSignInModal] = React.useState(false);
     return (
         <>
             <Navbar expand="lg">
@@ -37,21 +55,37 @@ const NavMenu = () => {
 
                             <hr style={{ width: "85%", height: "1px", border: "none", color: "#000", backgroundColor: "#000" }} />
                         </Nav.Item>
-                        <NavDropdown className="mx-auto px-3 navbar-item" title="Sam" id="profile-nav-dropdown">
-                            <NavDropdown.Item >
-                                <i className="fa fa-user-circle-o"> Samdeesh</i>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item as={ Link } to="user/">
-                                <i className="fa fa-gear"><span> Profile</span></i>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item as={ Link } to="/logout">
-                                <i className="fa fa-sign-out"><span> Logout</span></i>
-                            </NavDropdown.Item>
-                        </NavDropdown>
 
                         <Nav.Link className="mx-auto px-3" as={ Link } to="/cart">
                             <i className="fa fa-shopping-cart"><span className="navbar-item"> Cart</span></i>
                         </Nav.Link>
+
+                        <SignInModal show={showSignInModal} onHide={() => setShowSignInModal(false)}/>
+
+                        {
+                            userData ?
+                            (<DropdownButton
+                                menuAlign="right"
+                                title="Your Account"
+                                id="dropdown-menu-align-right"
+                                size="sm"
+                                className="p-1 mx-auto"
+                                variant="info"
+                                >
+                                    <Dropdown.Header className="text-capitalize"><i className="fa fa-user-circle-o"></i>{" "}{userFirstName}</Dropdown.Header>
+                                    <Dropdown.Item as={Link} to="/" ><i className="fa fa-gear"></i>{" "}Profile</Dropdown.Item>
+                                    <Dropdown.Divider />
+                                <Dropdown.Item as={Link} to="/" onClick={signOutHandler}><i className="fa fa-sign-out"></i>{" "}Sign out</Dropdown.Item>
+                            </DropdownButton>)
+                        :
+                        (
+                            <Nav.Item className="mx-auto p-1">
+                                <Button variant="info" size="sm" className="mx-2" onClick={() => setShowSignInModal(true)}>Sign in</Button>
+                            </Nav.Item>
+                        )
+
+                        }
+
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
