8ffb576dce2f8e82a541ecc16b7cf0e02f995ee5
Samdish <samdeeshsingh001@gmail.com>
2021-05-02 00:56:51 +0530

    refactors user controller and error handling using custom made ErrorResponse class

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -3,9 +3,10 @@ const { productValidator } = require("../helpers/dataValidation");
 const {
     productDataUpdateValidator,
 } = require("../helpers/updatedDataValidation");
+const ErrorResponse = require("../utils/errorResponse");
 
 // Get all Products [READ]
-const getProducts = async (req, res) => {
+const getProducts = async (req, res, next) => {
     try {
         var response = await Product.find().populate({
             path: "category",
@@ -13,10 +14,7 @@ const getProducts = async (req, res) => {
     } catch (error) {
         console.log("Some error  ocurred in promise");
         console.log(error);
-        return res.status(400).json({
-            status: 400,
-            message: "Bad request can not get the products",
-        });
+        return next(error);
     }
 
     // Return the food-items list in response.
@@ -27,7 +25,7 @@ const getProducts = async (req, res) => {
 };
 
 // Get all the products that by specifying product type.
-const getByProductType = async (req, res) => {
+const getByProductType = async (req, res, next) => {
     try {
         if (req.params.prodtype === "meals") {
             var response = await Product.find({
@@ -41,22 +39,27 @@ const getByProductType = async (req, res) => {
             }).populate({
                 path: "category",
             }); // Read all grocery-items from the DB.
+        } else {
+            return next(
+                new ErrorResponse(
+                    `Failed to get products for "${req.params.prodtype}"`,
+                    400
+                )
+            );
         }
 
         if (response.length === 0) {
-            return res.status(200).json({
-                success: false,
-                status: 404,
-                message: `Nothing found in the database for ${req.params.prodtype}`,
-            });
+            return next(
+                new ErrorResponse(
+                    `Nothing found in the database for ${req.params.prodype}`,
+                    404
+                )
+            );
         }
     } catch (error) {
         console.log("Some error  ocurred in promise");
         console.log(error);
-        return res.status(400).json({
-            status: 400,
-            message: "Bad request can not get the products",
-        });
+        return next(error);
     }
 
     // Return the food-items list in response.
@@ -67,7 +70,7 @@ const getByProductType = async (req, res) => {
 };
 
 // Get all the products by specifying a category and product type.
-const getProductsByCategory = async (req, res) => {
+const getProductsByCategory = async (req, res, next) => {
     const prodType = req.query.pt;
     const cat = req.query.category;
 
@@ -79,7 +82,7 @@ const getProductsByCategory = async (req, res) => {
         if (products.length !== 0) {
             products = products.filter((prod) => prod.category.name === cat);
 
-            if (product.length !== 0) {
+            if (products.length !== 0) {
                 return res.status(200).json({
                     success: true,
                     status: 200,
@@ -89,23 +92,17 @@ const getProductsByCategory = async (req, res) => {
         }
 
         if (products.length === 0) {
-            console.log("second if");
-            return res.status(404).json({
-                success: false,
-                message: `Nothing found for category ${cat}`,
-            });
+            return next(
+                new ErrorResponse(`Nothing found for category ${cat}`, 404)
+            );
         }
     } catch (error) {
-        return res.status(500).json({
-            status: 500,
-            error,
-            message: error.message,
-        });
+        return next(error);
     }
 };
 
 // Get/Read Product by specifying an id
-const getProductById = async (req, res) => {
+const getProductById = async (req, res, next) => {
     try {
         const productData = await Product.findById(req.params.id).populate({
             path: "category",
@@ -113,10 +110,12 @@ const getProductById = async (req, res) => {
 
         // Returns false in response if product is not found.
         if (!productData) {
-            return res.status(404).json({
-                success: false,
-                message: "Id doesn't exist. Please provide correct product id",
-            });
+            return next(
+                new ErrorResponse(
+                    `Id doesn't exist. Please provide correct product id`,
+                    404
+                )
+            );
         }
 
         // Returns response with data if product is found.
@@ -127,24 +126,21 @@ const getProductById = async (req, res) => {
     } catch (error) {
         // Catches errors if id is invalid, or some other issue raise.
         console.log(error);
-        return res.status(500).json({
-            success: false,
-            message:
-                "Could not find product! product id is not valid. please provide correct id",
-        });
+        return next(error);
     }
 };
 
 // Post Product [CREATE]
-const postProducts = async (req, res) => {
+const postProducts = async (req, res, next) => {
     // validate food-items input data.
     const { value, error } = productValidator(req.body);
     if (error) {
-        return res.status(406).json({
-            status: 406,
-            error: "ValidationError",
-            message: error.details[0].message,
-        });
+        return next(
+            new ErrorResponse(
+                `Please provide correct data! ${error.details[0].message}`,
+                406
+            )
+        );
     }
 
     // Create an object to save/create it in DB.
@@ -172,11 +168,7 @@ const postProducts = async (req, res) => {
     } catch (error) {
         console.log("Error in posting food item");
         console.log(error);
-        return res.status(500).json({
-            status: 500,
-            message: "something went wrong",
-            error,
-        });
+        return next(error);
     }
 
     // Return the success in response.
@@ -188,71 +180,68 @@ const postProducts = async (req, res) => {
 };
 
 // Delete a product by specifying an id. [DELETE]
-const deleteProductById = async (req, res) => {
+const deleteProductById = async (req, res, next) => {
     try {
         const productFound = await Product.findByIdAndDelete(req.params.id);
         // Returns false in response if product is not found.
         if (!productFound) {
-            return res.status(404).json({
-                success: false,
-                message:
-                    "ID you provided did not found. please specify correct id",
-            });
+            return next(
+                new ErrorResponse(
+                    `Provided ID ${req.params.id} did not found. please specify correct id`,
+                    404
+                )
+            );
         }
 
         // Returns success in response if product is found.
         if (productFound) {
             return res.status(200).json({
                 success: true,
+                status: 200,
                 message: "Product deleted successfully",
                 deletedProduct: productFound,
             });
         }
     } catch (error) {
         // Catches error if id is invalid, incorrect or some other error raise.
-        console.log(
-            "Some error ocurred may be because of invalid format for product id"
-        );
+        console.log("Some error ocurred while processing req");
         console.log(error);
-        return res.status(500).json({
-            success: false,
-            message:
-                "Something went wrong. Maybe invalid product id. Please provide correct product id",
-            error,
-        });
+        return next(error);
     }
 };
 
 // Put/Update a product in DB by specifying an id. [PUT]
-const putProductById = async (req, res) => {
+const putProductById = async (req, res, next) => {
     const dataToBeUpdated = req.body;
 
     // Updated data validation for Product, returns error if invalid.
     const { error } = productDataUpdateValidator(dataToBeUpdated);
     if (error) {
-        return res.status(400).json({
-            success: false,
-            message: "Please provide correct data format",
-            error,
-        });
+        return next(
+            new ErrorResponse(
+                `Please provide correct data format! ${error.details[0].message}`,
+                406
+            )
+        );
     }
 
     try {
-        const productFound = await Product.findById(req.params.id);
-        if (!productFound) {
-            // If product id doesn't exist then returns response with 404.
-            return res.status(404).json({
-                success: false,
-                message: "Product id not found please provide correct id",
-            });
-        }
-
         // Updates product by giving an id and returns success response.
         const updateProduct = await Product.findByIdAndUpdate(
             req.params.id,
             dataToBeUpdated,
             { new: true, useFindAndModify: false }
         );
+
+        if (!updateProduct) {
+            return next(
+                new ErrorResponse(
+                    `Product id "${req.params.id}" not found please provide correct id`,
+                    404
+                )
+            );
+        }
+
         return res.status(200).json({
             success: true,
             message: "Product information updated successfully",
@@ -260,12 +249,7 @@ const putProductById = async (req, res) => {
         });
     } catch (error) {
         // Catches errors if mongoDB ObjectId is invalid.
-        return res.status(500).json({
-            success: false,
-            message:
-                "Something went wrong while updating, could not update the product information",
-            error,
-        });
+        return next(error);
     }
 };
 
